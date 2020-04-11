c2c9a04d6396999a95d6038282419becd21cd064
mse-lunani <michael.se-lunani@riaraunniversity.ac.ke>
2020-04-11 10:57:41 +0300

    my commit Please enter the commit message for your changes. Lines starting

--- gettingstarted/resources/views/welcome.blade.php
+++ gettingstarted/resources/views/welcome.blade.php
@@ -1,21 +1,11 @@
 @extends('layouts.master')
 @section('content')
 <div class="row">
-    <div class="col-md-12">
-<h1>control structures</h1>
-{{ $r = rand(0,1)}}
-@if($r == 0)
-<p>this only displays if true</p>
-@else
-<p>this only displays if false</p>
-@endif
-@for($i = 0; $i<5 ; $i++)
-<p>{{ $i }}. Iteration</p>
-@endfor
-<hr>
-{{"<script>alert('this doesnt work') </script>"}}
+        <div class="col-md-12">
+            <p class="quote">Start of great things</p>
+        </div>
+    </div>
+<div>
 
-{!!"<script>alert('this works fine') </script>"!!}
-</div>
 </div>
 @endsection
