c8f25066f4b51b73a73ef7993927fcdad4750e0d
mse-lunani <michael.se-lunani@riaraunniversity.ac.ke>
2020-04-12 14:15:09 +0300

    my commit

--- gettingstarted/routes/web.php
+++ gettingstarted/routes/web.php
@@ -4,19 +4,19 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('blog.index');
-});
+})->name('blog.index');
 Route::get('/post{id}',function(){
 	return view('blog.post');
-});
-Route::get('about',function(){
+})->name('bolg.post');
+Route::get('/about',function(){
 return view('other.about');
-});
+})->name('about.index');
 Route::get('admin',function(){
 	return view('admin.index');
-});
-Route::get('admin/create',function(){
-	return view('admin.create')
-});
+})->name('admin.index');
+Route::get('admin/index',function(){
+	return view('admin.create');
+})->name('admin.create');
 Route::get('admin/edit{id}',function(){
-	return view('admin.create')
-});
+	return view('admin.edit');
+})->name('admin.edit');
